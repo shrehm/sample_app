25797e21235cad7714744618650db0e1517d2210
ShRehm <shrehm@hotmail.com>
2018-05-01 09:01:32 +0000

    Modified sqlite dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-   protect_from_forgery with: :exception
-
-  def hello
-    render html: "hello, world!"
-  end
+   root 'application#hello'
   
 end
